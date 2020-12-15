61c17e676cd1136c199807d5d11ecf0ca6bb26a1
ejnshtein <ejnshtein@gmail.com>
2020-12-15 19:44:32 +0200

    update stream example

--- example/stream-save-file.js
+++ example/stream-save-file.js
@@ -1,12 +1,16 @@
-import request from '../'
+import { request } from '../'
 import fs from 'fs'
 import path from 'path'
 
 request('https://picsum.photos/200/300', { responseType: 'headers' })
   .then(({ headers }) => request(headers.location, { responseType: 'stream' }))
   .then(({ data, headers }) => {
-    const [_, fileName] = headers['content-disposition'].match(/filename="(\S+)"/i)
-    const destination = fs.createWriteStream(path.resolve(process.cwd(), fileName))
+    const [_, fileName] = headers['content-disposition'].match(
+      /filename="(\S+)"/i
+    )
+    const destination = fs.createWriteStream(
+      path.resolve(process.cwd(), fileName)
+    )
     data.pipe(destination)
     data.once('finish', () => {
       console.log('file saved!')
